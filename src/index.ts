a50bb006fb4c77f79fc3aed0bb6b80cac96a881b
yyhhenry <yyhhenry@foxmail.com>
2023-10-27 22:08:47 +0800

    fix: endless refresh

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-export const US_BING_HOST = 'www.bing.com';
-export const CN_BING_HOST = 'cn.bing.com';
-export const MARK_NAME = 'us-bing-trigger';
-export const WAIT_TIME = 2000;
-export function resolveURL(url?: URL | string) {
+const US_BING_HOST = 'www.bing.com';
+const CN_BING_HOST = 'cn.bing.com';
+const MARK_NAME = 'us-bing-trigger';
+const WAIT_TIME = 2000;
+function resolveURL(url?: URL | string) {
   const here = window.location.href;
   const urlObj = new URL(url ?? here, here);
   return urlObj;
 }
-export function isCNBing(url?: URL | string) {
+function isCNBing(url?: URL | string) {
   const urlObj = resolveURL(url);
   return (
     urlObj.host === CN_BING_HOST ||
     (urlObj.host === US_BING_HOST && urlObj.searchParams.get('mkt') === 'zh-CN')
   );
 }
-export function toUSBingURL(url?: URL | string) {
+function toUSBingURL(url?: URL | string) {
   const urlObj = resolveURL(url);
   urlObj.host = US_BING_HOST;
   const q = urlObj.searchParams.get('q') ?? '';
@@ -29,25 +29,33 @@ export function toUSBingURL(url?: URL | string) {
 
   return urlObj;
 }
-export function resolveMark() {
+function hasMark() {
   const urlObj = resolveURL();
-  const hasMark = urlObj.searchParams.has(MARK_NAME);
-  if (hasMark) {
-    urlObj.searchParams.delete(MARK_NAME);
-    setTimeout(() => {
-      window.history.replaceState({}, '', urlObj);
-    }, WAIT_TIME);
-  }
-  return hasMark;
+  return urlObj.searchParams.has(MARK_NAME);
+}
+function asyncSleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+async function removeMark() {
+  const urlObj = resolveURL();
+  urlObj.searchParams.delete(MARK_NAME);
+  const newURL = urlObj.href;
+  await asyncSleep(WAIT_TIME);
+  window.history.replaceState({}, '', newURL);
 }
 
-export function resolveRedirecting() {
-  if (resolveMark()) {
-    return;
-  }
-  if (isCNBing()) {
+async function redirectUSBing() {
+  if (hasMark()) {
+    await removeMark();
+    if (isCNBing()) {
+      console.info("Seems like you don't have a VPN.");
+    } else {
+      console.info('Redirected to US Bing.');
+    }
+  } else if (isCNBing()) {
+    console.info('Redirecting to US Bing...');
     window.location.replace(toUSBingURL());
   }
 }
 
-resolveRedirecting();
+redirectUSBing();
